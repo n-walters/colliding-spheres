063c3d2c31aa1634baa2c052459ad66f070fa4f0
Nico <18425285+n-walters@users.noreply.github.com>
2018-02-28 16:57:44 +0100

    Added standard boilerplate canvas setup

--- canvas.js
+++ canvas.js
@@ -1,5 +1,26 @@
 "use strict"
 
+// Script is loaded in HTML HEAD, therefore before DOM elements.
 window.addEventListener("load", () => {
 	console.log("%cDOM loaded.", "color: green");
+	
+	const canvas = document.getElementById("canvas");
+	
+	context = init(canvas);
+	
+	animate(context);
 });
+
+let context;
+let animation;
+
+function init(canvas) {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	return canvas.getContext("2d");
+}
+
+function animate(context) {
+	animation = window.requestAnimationFrame(() => animate(context));
+	context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+}
